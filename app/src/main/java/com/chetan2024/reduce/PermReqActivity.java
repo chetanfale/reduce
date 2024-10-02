812e955ecf00c499f356b5853c507aef4719a298
Chetan Fale <imailchetan@gmail.com>
2024-10-02 17:52:02 +0530

    redirect to activity 2

--- app/src/main/java/com/chetan2024/reduce/PermReqActivity.java
+++ app/src/main/java/com/chetan2024/reduce/PermReqActivity.java
@@ -1,6 +1,22 @@
 package com.chetan2024.reduce;
 
-import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.view.View;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class PermReqActivity extends Activity {
+public class PermReqActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.permreq);
+    }
+
+    // Method to take the user to the Usage Access Settings page
+    public void goToSettings(View view) {
+        Intent intent = new Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS);
+        startActivity(intent);
+    }
 }
